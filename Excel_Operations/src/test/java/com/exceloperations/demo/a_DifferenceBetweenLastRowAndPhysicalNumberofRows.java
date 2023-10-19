d77d0f223fe93ef932ba3d0d4bbc9a083ed5bd12
DEADSHOT <suniljagadeesh2600443@gmail.com>
2023-10-19 15:11:44 +0530

    19-10-2023 15:11

---
+++ Excel_Operations/src/test/java/com/exceloperations/demo/a_DifferenceBetweenLastRowAndPhysicalNumberofRows.java
@@ -0,0 +1,32 @@
+package com.exceloperations.demo;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class a_DifferenceBetweenLastRowAndPhysicalNumberofRows {
+	@SuppressWarnings("unlikely-arg-type")
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis = new FileInputStream("D:\\Advance Selenium\\Excel_Operations\\data\\Book.xlsx");
+
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+
+		int numberOfSheets = workbook.getNumberOfSheets();
+
+		for (int i = 0; i < numberOfSheets; i++) {
+			if ((workbook.getSheetName(i)).equals("Sheet1")) {
+				XSSFSheet sheet = workbook.getSheetAt(i);
+				int numberOfRows = sheet.getLastRowNum();
+				int numberOfRows1 = sheet.getPhysicalNumberOfRows();
+				int numberOfCells = sheet.getRow(0).getLastCellNum();
+				int numberOfCells1 = sheet.getRow(0).getPhysicalNumberOfCells();
+				System.out.println(numberOfRows);// 5
+				System.out.println(numberOfRows1);// 6
+				System.out.println(numberOfCells);// 2
+				System.out.println(numberOfCells1);// 2
+			}
+		}
+	}
+}
